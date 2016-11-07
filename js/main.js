464e36a0448d99e4c2f9e9728d6e383040752e4f
havidlearner <jhreina@yahoo.com>
2016-11-07 11:05:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+// resize method borrowed from https://gist.github.com/aarongustafson/4157402
 
+(function( window ){
+    window.watchResize = function( callback ){
+        var resizing;
+        callback.size = 0;
+        function done()
+        {
+            var curr_size = window.innerWidth;
+            clearTimeout( resizing );
+            resizing = null;
+            // only run on a true resize
+            if ( callback.size != curr_size )
+            {
+                callback();
+                callback.size = curr_size;
+            }
+        }
+        window.addEventListener('resize', function(){
+            if ( resizing )
+            {
+                clearTimeout( resizing );
+                resizing = null;
+            }
+            resizing = setTimeout( done, 50 );
+        });
+        // init
+        callback();
+    };
+}(window));
